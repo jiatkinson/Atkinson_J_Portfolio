400841cc91ad86d3a33167e618ade547ac2e88d3
Jacob Atkinson <jiatkinson@Jacobs-MBP-2.lan>
2018-11-23 21:56:09 -0500

    I actually don't know what I changed...

--- data/data.js
+++
@@ -1,9 +0,0 @@
-var jacobData = {
-videos: ['<iframe src="https://www.youtube.com/embed/kDK-dTcg6J0?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
-'<iframe src="https://www.youtube.com/embed/6ddNGw53AWw?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
-'<iframe src="https://www.youtube.com/embed/_J1llAPa2bo?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'],
-
-projectDesc: ['Conquer The Coast | Ontario’s Blue Coast - Editor',
-          'Hessenland Country Inn Promotional Video',
-            'On My High – Passion Project']
-};
